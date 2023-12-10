fb73d33206ab1c456e6c73906f784e3bd342e97d
Valentin-1981 <chibinyov@rambler.ru>
2023-12-11 01:46:19 +0300

    first commit

---
+++ file_1.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.base import runTouchApp
+from kivy.lang import Builder
+
+runTouchApp(Builder.load_string('''
+    
+BoxLayout:
+    Button:
+        text: 'Bl' 
+    
+'''))
---
+++ file_2.py
@@ -0,0 +1,11 @@
+from kivy.app import App
+from kivy.base import runTouchApp
+from kivy.lang import Builder
+
+runTouchApp(Builder.load_string('''
+
+StackLayout:
+    Button:
+        text: 'kivy'
+        size_hint: .2,.2
+'''))
